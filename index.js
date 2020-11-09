ce9f67b70c7f49d4daad98fccf3de215779d58a7
Roman-Shashkov <romadooms@gmail.com>
2020-11-09 14:53:03 +0300

    add functions

--- index.js
+++ index.js
@@ -34,6 +34,99 @@ function startGame(fileldSize) {
         }
     }
 
+    //передвижение плиток вправо
+
+    function swipeRight() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let rowArr = [+totalOne, +totalTwo, +totalThree, +totalFour]
+
+                let filteredRowArr = rowArr.filter (num => num)
+
+                let empty = 4 - filteredRowArr.length
+                let zeroes = Array(empty).fill('')
+
+                let newRowArr = zeroes.concat(filteredRowArr)
+
+                squares[i].innerHTML = newRowArr [0]
+                squares[i + 1].innerHTML = newRowArr [1]
+                squares[i + 2].innerHTML = newRowArr [2]
+                squares[i + 3].innerHTML = newRowArr [3]
+            }
+        }
+    } 
+
+    //передвижение плиток влево
+
+    function swipeLeft() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let rowArr = [+totalOne, +totalTwo, +totalThree, +totalFour]
+
+                let filteredRowArr = rowArr.filter (num => num)
+
+                let empty = 4 - filteredRowArr.length
+                let zeroes = Array(empty).fill('')
+
+                let newRowArr = filteredRowArr.concat(zeroes)
+
+                squares[i].innerHTML = newRowArr [0]
+                squares[i + 1].innerHTML = newRowArr [1]
+                squares[i + 2].innerHTML = newRowArr [2]
+                squares[i + 3].innerHTML = newRowArr [3]
+            }
+        }
+    } 
+
+    //сложение строк
+
+    function concatRow () {
+        for (let i = 0; i < 15; i++) {
+            if ((i % 4 !== 3) && squares[i].innerHTML === squares[i + 1].innerHTML) {
+                let concatTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i + 1].innerHTML)
+                squares[i].innerHTML = concatTotal
+                squares[i + 1].innerHTML = 0
+            }
+        }
+    }
+
+    //назначение клавиш
+
+    function check(e) {
+        if(e.key === 'ArrowRight' || e.key === 'd') {
+            keyRight()
+        } else if (e.key === 'ArrowLeft' || e.key === 'a') {
+            keyLeft()
+        }
+    }
+   
+    document.addEventListener('keyup', check)
+
+    function keyRight() {
+        swipeRight()
+        concatRow()
+        swipeRight()
+        generateRandomNumbers()
+    }
+
+    function keyLeft() {
+        swipeLeft()
+        concatRow()
+        swipeLeft()
+        generateRandomNumbers()
+    }
+
+
+
+
 
 
 
